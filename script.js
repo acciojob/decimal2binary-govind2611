9decba12602930ad6deeb5ec0fa122a7cacaa3f4
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-10-05 07:34:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
-}
+  if (num < 0 || !Number.isInteger(num)) {
+    return 'Invalid input. Please enter a non-negative integer.';
+  }
 
-window.decimalToBinary = decimalToBinary;
+  if (num === 0) {
+    return '0';
+  }
+
+  let binary = '';
+  while (num > 0) {
+    binary = (num % 2) + binary;
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
+}
